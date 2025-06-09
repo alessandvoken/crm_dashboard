07917c80c024d7a0a5ebbb21cf4697f1599e3b5f
Alessandro Perchiazzi <126801601+alessandvoken@users.noreply.github.com>
2025-06-09 15:36:23 +0200

    Feat: theme.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./theme";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Dashboard />
+    </ThemeProvider>
+  );
 }
 
 export default App;
